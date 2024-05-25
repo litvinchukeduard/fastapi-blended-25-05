feb920714abbc7c93c09b9d8ac2d2393aeea5e23
Edward Litvinchuck <elitvinchuck@gmail.com>
2024-05-25 19:09:14 +0300

    Recipe ingredient association table

--- src/database/models.py
+++ src/database/models.py
@@ -1,7 +1,19 @@
+from typing import List
+
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import String, Integer, Column
+from sqlalchemy import ForeignKey, String, Integer, Column
+from sqlalchemy import Table
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
+recipe_ingredient_association_table = Table(
+    "recipe_ingredient_association_table",
+    Base.metadata,
+    Column("recipe_id", ForeignKey("recipes.id")),
+    Column("ingredient_id", ForeignKey("ingredients.id")),
+)
 
 #Recipe (id, name, description, ingredients[])
 class Recipe(Base):
@@ -10,6 +22,7 @@ class Recipe(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False, unique=True)
     description = Column(String(255), nullable=False)
+    ingredients: Mapped[List['Ingredient']] = relationship(secondary=recipe_ingredient_association_table)
 
 # Ingredient (id, name, description)
 
